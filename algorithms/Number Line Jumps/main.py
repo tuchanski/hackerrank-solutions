db4c6d3218ab382415c8d19b1df5081374e81067
Tuchanski <gui.rocha2004@gmail.com>
2024-10-04 15:11:18 -0300

    Adiciona Number Line Jumps

---
+++ algorithms/Number Line Jumps/main.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    
+    pos_k1 = x1
+    pos_k2 = x2
+    at_same_locale = False
+
+    for i in range(10000):
+        pos_k1 += v1
+        pos_k2 += v2
+        if pos_k1 == pos_k2:
+            at_same_locale = True
+    
+    if at_same_locale:
+        return "YES"
+        
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
